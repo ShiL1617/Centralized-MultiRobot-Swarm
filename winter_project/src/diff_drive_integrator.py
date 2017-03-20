29b9a87cd431c0bacc4612574270119154340459
LukeS <lukeshi2017@u.northwestern.edu>
2017-03-19 23:26:04 -0500

    renamed to shape_formation_integrator.py for less ambiguity

--- winter_project/src/diff_drive_integrator.py
+++ winter_project/src/diff_drive_integrator.py
@@ -7,12 +7,13 @@ import rospy
 import tf
 import tf.transformations as tr
 from geometry_msgs.msg import Twist, Pose2D
+from winter_project.msg import multi_poses, multi_vels
 
 ###################
 # NON ROS IMPORTS #
 ###################
 import numpy as np
-from math import pi, atan, sin, cos
+from math import pi, atan, sin, cos, sqrt
 
 ####################
 # GLOBAL CONSTANTS #
@@ -20,72 +21,99 @@ from math import pi, atan, sin, cos
 PARENT_FRAME = "odom"
 CHILD_FRAME = "robot_1"
 INTEGRATION_FREQ = 250 # Hz
-INITIAL_POSE = np.array([2., 0., pi/3.])
-INTEGRATOR_NUM = 0
+# INITIAL_POSES = np.array([[1.,0.,0.]])
+N = 10
+
+# circle arrangement for 10 robots
+INITIAL_POSES = np.array([[3., 0., 0.]
+    ,[3.*sqrt(2.)/2.,3.*sqrt(2.)/2.,0.],
+    [0., 3., 0.],
+    [-3.*sqrt(2.)/2.,3.*sqrt(2.)/2.,0.],
+    [-3., 0., 0.],
+    [3.*cos(7.*pi/6.), 3*sin(7.*pi/6.), 0.],
+    [3.*cos(4.*pi/3.), 3*sin(4.*pi/3.), 0.],
+    [0., -3., 0.],
+    [3.*cos(5.*pi/3), 3.*sin(5.*pi/3.), 0.],
+    [3.*cos(11.*pi/6.), 3.*sin(11.*pi/6.), 0.]])
+
+
+# star arrangement for 10 robots
+# INITIAL_POSES = np.array([[-1., 0., 0.],[1., 0., 0.],
+#     [0.5*sqrt(2.)/2.,0.5*sqrt(2.)/2.,0.],
+#     [0., 1., 0.],
+#     [-0.5*sqrt(2.)/2.,0.5*sqrt(2.)/2.,0.],
+#     [1.*cos(4.*pi/3.), 1*sin(4.*pi/3.), 0.],
+#     [0.5*cos(7.*pi/6.), 0.5*sin(7.*pi/6.), 0.],
+#     [0., -0.5, 0.],
+#     [1.*cos(5.*pi/3), 1.*sin(5.*pi/3.), 0.],
+#     [0.5*cos(11.*pi/6.), 0.5*sin(11.*pi/6.), 0.]])
+
+# cross arrangement for 10 robots
+# INITIAL_POSES = np.array([[0., 1., 0.],[0., 0.5, 0.],[-1., 0., 0.],[-0.5, 0., 0.],[0., 0., 0.],[0.5, 0., 0.],[1., 0., 0.],[0., -0.5, 0.],[0., -1., 0.],[0., -2., 0.]])
+
+# INITIAL_POSES = np.array([[-3., 3., 0.],[3., 3., 0.]])
 
 # CREATE MAIN CLASS:
 class DiffDriveIntegrator( object ):
-    def __init__(self, INITIAL_POSE):
+    def __init__(self):
         #rospy.loginfo("Creating DiffDriveIntegrator class!")
 
         # read all parameters:
         self.parent = rospy.get_param("~parent", PARENT_FRAME)
         self.child = rospy.get_param("~child", CHILD_FRAME)
         self.freq = rospy.get_param("~freq", INTEGRATION_FREQ)
-        #self.initpose = rospy.get_param("~initpose", INITIAL_POSE)
-        self.integrator_num = rospy.get_param("~integrator_num", INTEGRATOR_NUM)
 
         # create random variables that we are going to need:
-        self.current_pose = INITIAL_POSE[self.integrator_num]
-        self.current_vel = np.zeros(2)
-        self.camera_est_pose = np.array([0.,0.,0.])
+        self.current_vel = np.zeros((N,2))
+        self.current_pose = INITIAL_POSES
+        self.n_poses = multi_poses()
 
+        self.init_flag = False
+
+        # self.size_flag = True
         # create all subscribers, timers, publishers, listeners, broadcasters,
         # etc.
         self.br = tf.TransformBroadcaster()
-        self.pos_pub = rospy.Publisher("pose_est", Pose2D, queue_size=10)
-        self.rate = rospy.Rate(250)
-        self.cmd_sub = rospy.Subscriber("cmd_vel", Twist, self.twist_callback)
+        self.pos_pub = rospy.Publisher("pose_est", multi_poses, queue_size=10)
+        self.cmd_sub = rospy.Subscriber("cmd_vel", multi_vels, self.twist_callback)
         self.int_timer = rospy.Timer(rospy.Duration(1/float(self.freq)), self.integrate_callback)
         return
 
-    def integrate_callback(self, tdat):
-        v = self.current_vel[0]
-        w = self.current_vel[1]
+    def integrate_callback(self, tdata):
+        self.n_poses = multi_poses()
+        #function
+        # print self.current_vel
+        for i in range(N):
 
-        self.current_pose += (1/float(self.freq))*np.array([
-            v*np.cos(self.current_pose)[2],
-            v*np.sin(self.current_pose)[2],
-            w])
+            #casting iteration counter as integer
+            # i = i[0]
 
-        #noise from integration (lack of accurate encoders/odometry)
+            v = self.current_vel[i][0]
+            w = self.current_vel[i][1]
 
-        #noise parameters, as seen in odometry model in Probabilistic Robotics
-        a1 = 0.01
-        a2 = 0.01
-        a3 = 0.01
-        a4 = 0.01
+            # if not self.init_flag:
+            #     self.current_pose[i] = INITIAL_POSES[i]
+            #     self.init_flag = True
 
-        noise_linear = np.random.normal(0,0.001,1)
-        noise_angular1 = np.random.normal(0,0.001,1)
-        noise_angular2 = np.random.normal(0,0.001,1)
-        #noise from camera (from camera_noise callback)
+            # print "INTEGRATOR NODE POSE", self.current_pose[i]
+            # print type(self.current_pose[0])
+            # print "self.current_pose[2]", self.current_pose[0][2]
 
+            self.current_pose[i] += (1/float(self.freq))*np.array([
+                v*np.cos(self.current_pose[i][2]),
+                v*np.sin(self.current_pose[i][2]),
+                w])
 
-        #final pose estimate from Kalman filter (to be added later)
+            # let's send that pose data out as a tf message:
+            pos, quat = self.pose_arr_to_tf(self.current_pose[i])
+            self.child = "robot"+"_"+str(i)
+            self.br.sendTransform(pos, quat, rospy.Time.now(), self.child, self.parent)
 
+            #publish estimated pose to diffdrive_velocity controller via pose_est topic
 
-        #publish estimated pose from noise to diffdrive_velocity controller via pose_est topic
-        p_est = Pose2D()
-        p_est.x = self.current_pose[0]
-        p_est.y = self.current_pose[1]
-        p_est.theta = self.current_pose[2]
-
-        self.pos_pub.publish(p_est)
-
-        # now let's send that pose data out as a tf message:
-        pos, quat = self.pose_arr_to_tf(self.current_pose)
-        self.br.sendTransform(pos, quat, rospy.Time.now(), self.child, self.parent)
+            self.n_poses.poses.append(Pose2D(self.current_pose[i][0],self.current_pose[i][1],self.current_pose[i][2]))
+            # self.init_flag = False
+        self.pos_pub.publish(self.n_poses)
 
         return
 
@@ -98,21 +126,18 @@ class DiffDriveIntegrator( object ):
         quat = tr.quaternion_from_euler(0, 0, pose[2], 'sxyz')
         return pos, quat
 
-    def twist_to_vel_arr(self, twist):
-        vel = np.array([twist.linear.x, twist.angular.z])
+    def twist_to_vel_arr(self, multi):
+        vel = np.zeros((N,2))
+        for i in range(N):
+            vel[i] = np.array([multi.twists[i].linear.x, multi.twists[i].angular.z])
         return vel
 
 
 def main():
     rospy.init_node('diff_drive_integrator', log_level=rospy.INFO)
 
-    INITIAL_POSES = np.array([[-2.,0.,0.],[0.,0.,0.],[2.,0.,0.]])
-
     try:
-        integrator = DiffDriveIntegrator(INITIAL_POSES)
-        #integrator2 = DiffDriveIntegrator(INITIAL_POSES)
-        #integrator3 = DiffDriveIntegrator(INITIAL_POSES[2])
-
+        diffdrive = DiffDriveIntegrator()
 
     except rospy.ROSInterruptException:
         pass
